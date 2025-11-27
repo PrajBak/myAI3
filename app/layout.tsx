e2bbf98b1bc28913da43fbf58ac84da3df47e955
PrajBak <prajwalbakshi@yahoo.com>
2025-11-27 19:02:00 +0530

    Changing tab name

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "MyAI3",
-  description: "MyAI3",
+  title: "InsightSkin",
+  description: "InsightSkin",
 };
 
 export default function RootLayout({
